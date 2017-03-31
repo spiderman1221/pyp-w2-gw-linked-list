9b2d4b5788190e09d3ad9f499980c6d33e9d4627
Michael Netherland <michaeljnetherland@gmail.com>
2017-03-31 05:06:02 +0000

    Initial Commit

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -1,5 +1,5 @@
 from .interface import AbstractLinkedList
-
+from .node import *
 
 class LinkedList(AbstractLinkedList):
     """
@@ -7,34 +7,125 @@ class LinkedList(AbstractLinkedList):
     """
 
     def __init__(self, elements=None):
-        pass
+        self.start = None
+        self.end = None
+        
+        if elements:
+            for value in elements:
+                self.append(value)
+       
+        
 
     def __str__(self):
-        pass
+        if self is None:
+            return "[]"
+        else:
+            result = []
+            for item in self:
+                result.append(item.elem)
+            return "{}".format(result)
 
     def __len__(self):
-        pass
+        counter = 0
+        for el in self:
+            counter += 1
+        return counter
 
     def __iter__(self):
-        pass
+        current = self.start
+        while current:
+            yield current
+            current = current.next
+        raise StopIteration
 
     def __getitem__(self, index):
-        pass
+        if index > len(self):
+            raise IndexError
+        if len(self) == 0:
+            raise IndexError
+        for ticker, item in enumerate(self):
+            if ticker == index:
+                return item.elem
+            
+        
+        
 
     def __add__(self, other):
-        pass
+        newlist = LinkedList()
+        for item in self:
+            newlist.append(item.elem)
+        
+        for item in other:
+            newlist.append(item.elem)
+        return newlist
+
+        
 
     def __iadd__(self, other):
-        pass
+        for item in other:
+            self.append(item.elem)
+        return self
 
     def __eq__(self, other):
-        pass
+        
+        print(self)
+        print(other)
+        if len(self) == len(other):
+            for items in zip(self, other):
+                if items[0].elem != items[1].elem:
+                    return False
+            return True
+        return False
+        
 
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+        
     def append(self, elem):
-        pass
-
+        if self.start is None:
+            self.start = Node(elem)
+            self.end = self.start
+        else:
+            new_node = Node(elem)
+            self.end.next = new_node
+            self.end = new_node
+        
+            
     def count(self):
-        pass
+        return len(self)
 
     def pop(self, index=None):
-        pass
+        
+        if index >= len(self):
+            raise IndexError
+        if len(self) == 0:
+            raise IndexError
+        if index is None:
+            index = len(self)-1
+        previous = None 
+        current = self.start
+        ticker = 0
+        while current:
+            
+            if ticker == index:
+                if previous is None:
+                    self.start = self.start.next
+                    return current.elem
+                
+                if current.next is None:
+                    previous.next = None
+                    self.end = previous
+                    return current.elem
+                
+                previous.next = current.next
+                return current.elem
+            ticker += 1
+            previous = current
+            current = current.next
+            
+            
+            
+            
+            
+            
--- linked_list/node.py
+++ linked_list/node.py
@@ -2,15 +2,21 @@ class Node(object):
     """
     Node class representing each of the linked nodes in the list.
     """
-
+    
     def __init__(self, elem, next=None):
-        pass
+        self.elem = elem
+        self.next = next
 
     def __str__(self):
-        pass
-
+        if self.next:
+            return "Node({}) > Node({})".format(self.elem, self.next.elem)  # 'Node(9) > Node(Node(X) > /)'
+        else:
+            return "Node({}) > /".format(self.elem)
+        
     def __eq__(self, other):
-        pass
+        return self.elem  == other.elem and self.next == other.next
 
     def __repr__(self):
-        pass
+        return str(self)
+
+
